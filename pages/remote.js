cfce647ae147fe9cff27c9da97ad25afa4f63737
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-10-08 01:30:22 +0800

    Add remote button

---
+++ pages/remote.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function remote () {
+  return (
+    <div className='d-flex j-content-center'>
+      <button className='btn-correct'>Correct</button>
+      <button className='btn-wrong'>Wrong</button>
+      <button className='btn-previous'>Previous</button>
+      <button className='btn-next'>Next</button>
+    </div>
+  )
+}
