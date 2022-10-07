af0c4a6acbdf3f7c5e2d12c189166e7b19ceda20
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-10-08 05:13:24 +0800

    Fix leaderboard empty on load bug

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Leaderboard ({ topPlayers }) {
-  console.log(topPlayers)
+  if(!topPlayers.length) return
   return (
     <div className='leaderboard-area'>
       <table>
