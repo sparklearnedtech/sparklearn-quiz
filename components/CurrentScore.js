c820e6aaa8822c77317e98921cf584ad2eaa72f1
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-10-08 03:13:27 +0800

    Modify current score component

--- components/CurrentScore.js
+++ components/CurrentScore.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function FinalScore ({ nickname, score }) {
   return (
-    <div className='final-score-area'>
+    <div className='current-score-area'>
       <h2 className='head-nickname'>{nickname}</h2>
       <div className='d-flex j-content-end'>
         <h2 className='head-score'>{score}</h2>
