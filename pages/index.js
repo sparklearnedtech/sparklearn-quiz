76aae34b5d981fb37e91b3dbadc5af3030e98bca
Christian Salvadora <christianrsalvadora@gmail.com>
2022-10-09 10:28:16 +0800

    Enhance remeining time text

--- pages/index.js
+++ pages/index.js
@@ -57,8 +57,22 @@ export default function Home ({ questions }) {
           console.log('Wrong')
         }}
       >
-        {({ remainingTime }) => remainingTime}
+        {renderTime}
       </CountdownCircleTimer>
+    )
+  }
+
+  const renderTime = ({ remainingTime }) => {
+    if (remainingTime === 0) {
+      return <div className='timer'>Too lale...</div>
+    }
+
+    return (
+      <div className='timer'>
+        <div className='text'>Remaining</div>
+        <div className='value'>{remainingTime}</div>
+        <div className='text'>seconds</div>
+      </div>
     )
   }
 
